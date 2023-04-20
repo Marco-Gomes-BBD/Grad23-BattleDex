c7487ef3a66d858854c5dd1840db1dbd188571a8
Daniel Trevor Harding <danielha@bbd.co.za>
2023-04-20 15:59:27 +0200

    using db function

--- SlideGenerator/SlideGenerator.cs
+++ SlideGenerator/SlideGenerator.cs
@@ -1,51 +1,20 @@
+using Grad23_BattleDex.Database;
 using System.Data;
-using System.Data.SqlClient;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
 
 namespace Grad23_BattleDex.SG
 {
 
     public class SlideGenerator
     {
-        private const string query = "SELECT DISTINC(IMAGES.file_path)" +
-                        "FROM images_tags " +
-                        "INNER JOIN tags ON tags.id = images_tags.tag_id " +
-                        "INNER JOIN images ON images.id = images_tags.image_id" +
-                        "WHERE TAGS.tag IN @tags";
         private static Random random = new();
 
-        public static List<string> Generate(List<string> tags, int presentationSize, SqlConnection connection)
+        public static List<string> Generate(List<string> tags, int presentationSize)
         {
-            return GetImageLocations(tags, connection)
+            DatabaseFunctions databaseFunctions = new DatabaseFunctions();
+            return databaseFunctions.GetImagesForTags(tags)
                 .OrderBy(a => random.Next())
                 .Take(presentationSize)
                 .ToList();
-        }
-
-        private static List<string> GetImageLocations(List<string> tags, SqlConnection connection)
-        {
-            SqlCommand sqlCommand = new(query, connection);
-            sqlCommand.Parameters.AddWithValue("@tags", tags);
-            try
-            {
-                SqlDataReader reader = sqlCommand.ExecuteReader();
-                List<string> locations = GetEnumerable(reader).ToList();
-                reader.Close();
-                return locations;
-            } catch (Exception ex) 
-            { 
-                Console.WriteLine(ex.Message);
-            }
-            return new List<string>();
-        }
-
-        private static IEnumerable<string> GetEnumerable(SqlDataReader reader)
-        {
-            while (reader.Read())
-            {
-                yield return reader.GetString(0);
-            }
         }
 
     }
