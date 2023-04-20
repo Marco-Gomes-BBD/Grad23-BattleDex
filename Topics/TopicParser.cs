86372b728f6357093f40674d8e3ae654fc2250ee
Marco Gomes <marcog@bbd.co.za>
2023-04-20 14:25:37 +0200

    Add topic parser

---
+++ Topics/TopicParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using Grad23_BattleDex.Language;
+
+namespace Grad23_BattleDex.Topics
+{
+    internal class TopicParser
+    {
+        public static string[] Parse(string topic)
+        {
+            Regex wordMatcher = new(@"\w+");
+            MatchCollection matches = wordMatcher.Matches(topic);
+
+            IEnumerable<string> words = matches.Select(match => match.Value);
+            IEnumerable<string> tags = words.Select(word => Stemmer.Stem(word));
+
+            return tags.ToArray();
+        }
+    }
+}
