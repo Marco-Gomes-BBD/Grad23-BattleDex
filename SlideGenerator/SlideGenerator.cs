3ed68737b8307ecbdab39780e1cf8900de8373a1
Daniel Trevor Harding <danielha@bbd.co.za>
2023-04-20 11:04:49 +0200

    Finishing off basic functionality

--- SlideGenerator/SlideGenerator.cs
+++ SlideGenerator/SlideGenerator.cs
@@ -1,4 +1,9 @@
-namespace Grad23_BattleDex.SlideGenerator
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using WinFormsApp1;
+
+namespace Grad23_BattleDex.SG
 {
 
     public class SlideGenerator
@@ -6,28 +11,40 @@ namespace Grad23_BattleDex.SlideGenerator
 
         private static Random random = new Random();
 
-        public List<Bitmap> Generate(List<string> tags, int presentationSize)
+        public List<string> Generate(List<string> tags, int presentationSize, SqlConnection connection)
         {
-            List<Bitmap> locations = GetImageLocations(tags)
+            List<string> locations = GetImageLocations(tags, connection)
                 .OrderBy(a => random.Next())
                 .Take(presentationSize)
-                .Select(GetImage)
                 .ToList();
             return locations;
         }
 
-        private List<string> GetImageLocations(List<string> tags)
+        private List<string> GetImageLocations(List<string> tags, SqlConnection connection)
         {
-
+            List<string> locations = new List<string>();
+            string query = "SELECT DISTINC(IMAGES.file_path)" +
+                "FROM images_tags " +
+                "INNER JOIN tags ON tags.id = images_tags.tag_id " +
+                "INNER JOIN images ON images.id = images_tags.image_id" +
+                "WHERE TAGS.tag IN @tags";
+            SqlCommand sqlCommand = new SqlCommand(query, connection);
+            sqlCommand.Parameters.AddWithValue("@tags", tags);
+            try
+            {
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read()) 
+                {
+                    locations.Add(reader[0].ToString());
+                }
+                reader.Close();
+            } catch (Exception ex)
+            {
+                // TODO do something with the error
+                Console.WriteLine(ex.ToString());
+            }
+            return locations;
         }
-
-        private Bitmap GetImage(string fileLocations)
-        {
-            return new Bitmap(fileLocations);
-        }
-
-
-
 
     }
 
