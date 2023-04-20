4165d63fd6e319bdf36da6176c0c98b5a2abfd6e
Daniel Trevor Harding <danielha@bbd.co.za>
2023-04-20 13:16:54 +0200

    Const string for db query

--- SlideGenerator/SlideGenerator.cs
+++ SlideGenerator/SlideGenerator.cs
@@ -1,14 +1,18 @@
 using System;
+using System.CodeDom;
 using System.Data;
 using System.Data.SqlClient;
-using WinFormsApp1;
 
 namespace Grad23_BattleDex.SG
 {
 
     public class SlideGenerator
     {
-
+        private const string query = "SELECT DISTINC(IMAGES.file_path)" +
+                        "FROM images_tags " +
+                        "INNER JOIN tags ON tags.id = images_tags.tag_id " +
+                        "INNER JOIN images ON images.id = images_tags.image_id" +
+                        "WHERE TAGS.tag IN @tags";
         private static Random random = new Random();
 
         public static List<string> Generate(List<string> tags, int presentationSize, SqlConnection connection)
@@ -23,11 +27,6 @@ namespace Grad23_BattleDex.SG
         private static List<string> GetImageLocations(List<string> tags, SqlConnection connection)
         {
             List<string> locations = new List<string>();
-            string query = "SELECT DISTINC(IMAGES.file_path)" +
-                "FROM images_tags " +
-                "INNER JOIN tags ON tags.id = images_tags.tag_id " +
-                "INNER JOIN images ON images.id = images_tags.image_id" +
-                "WHERE TAGS.tag IN @tags";
             SqlCommand sqlCommand = new SqlCommand(query, connection);
             sqlCommand.Parameters.AddWithValue("@tags", tags);
             try
@@ -35,7 +34,7 @@ namespace Grad23_BattleDex.SG
                 SqlDataReader reader = sqlCommand.ExecuteReader();
                 while (reader.Read()) 
                 {
-                    locations.Add(reader[0].ToString());
+                    locations.Add(reader.GetString(0));
                 }
                 reader.Close();
             } catch (Exception ex)
